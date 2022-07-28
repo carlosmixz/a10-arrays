f4b051c66ebbedbd862067b662fb0377f7cf61ee
carlosmixz <carlosmixz3@gmail.com>
2022-07-28 18:14:17 -0300

    Disney

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let array1 = [8, 9, 15, 3, 78, 45, 2, 1, 0];
+let array2 = ["Disney", "Marvel", "DC", "Star Wars"];
+let array3 = [1, 2, 3, "Disney", "Marvel", "DC", "Star Wars", true, true];
+
+console.log(array1);
+console.log(array2);
+console.log(array3);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+let array1 = [8, 9, 15, 3, 78, 45, 2, 1, 0];
+let array2 = ["Disney", "Marvel", "DC"];
+let array3 = [1, 2, 3, "Disney", "Marvel", "DC", "Star Wars", true, true];
+
+console.log("Tamanho do array1: " + array1.length);
+console.log("Tamanho do array2: " + array2.length);
+console.log("Tamanho do array3: " + array3.length);
+
+console.log(array1[0]);
+console.log(array2[1]);
+console.log(array3[2]);
+
+console.log(array1.includes(8));
+console.log(array2.includes("Star Wars"));
